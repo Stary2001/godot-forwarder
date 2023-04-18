6103ba2dc5dc0c5f5f0e21a2dd43bc0c822a08a5
Caroline Joy Bell <carolinejoybell@gmail.com>
2023-04-18 12:29:53 -0700

    Update input with new libnx stuff
    Fixes the build on newest toolchain

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #include <switch.h>
 
 // for getcwd
@@ -17,41 +17,38 @@ typedef struct pack_file_entry {
 	char md5[16];
 } pack_file_entry;
 
-void bail(const char *error)
-{
+void bail(const char *error) {
+	PadState pad;
+	padInitializeDefault(&pad);
+
 	consoleInit(NULL);
 	printf("%s\npress + to exit\n", error);
-	padConfigureInput(1, HidNpadStyleSet_NpadStandard);
-	PadState pad;
-    padInitializeDefault(&pad);
+	while (appletMainLoop()) {
+		padUpdate(&pad);
 
-    while (appletMainLoop())
-    {
-	padUpdate(&pad);
-	u64 kDown = padGetButtonsDown(&pad);
+		if (padGetButtonsDown(&pad) & HidNpadButton_Plus)
+			break;
 
-        if (kDown & HidNpadButton_Plus)
-            break;
-        consoleUpdate(NULL);
-    }
-    consoleExit(NULL);
+		consoleUpdate(NULL);
+	}
+	consoleExit(NULL);
 }
 
-uint32_t read_32(FILE* f) {
+uint32_t read_32(FILE *f) {
 	uint32_t n;
 	fread(&n, 1, 4, f);
 	return n;
 }
 
-uint64_t read_64(FILE* f) {
+uint64_t read_64(FILE *f) {
 	uint64_t n;
 	fread(&n, 1, 8, f);
 	return n;
 }
 
-struct pack_file_entry* read_file_entry(FILE *f, struct pack_file_entry *entry) {
+struct pack_file_entry *read_file_entry(FILE *f, struct pack_file_entry *entry) {
 	uint32_t name_len = read_32(f);
-	char *filename = malloc(name_len+1);
+	char *filename = malloc(name_len + 1);
 	fread(filename, 1, name_len, f);
 	filename[name_len] = 0;
 
@@ -65,17 +62,17 @@ struct pack_file_entry* read_file_entry(FILE *f, struct pack_file_entry *entry) 
 
 bool file_exists(const char *path) {
 	FILE *f = fopen(path, "rb");
-	if(f != NULL) {
+	if (f != NULL) {
 		fclose(f);
 		return true;
 	}
 	return false;
 }
 
-void invoke_with_merged_argv(const char *nro_path, int argc, char* argv[]) {
+void invoke_with_merged_argv(const char *nro_path, int argc, char *argv[]) {
 	size_t argv_len = strlen(nro_path) + 1;
 	// skip argv[0]
-	for(int i = 1; i < argc; i++) {
+	for (int i = 1; i < argc; i++) {
 		argv_len += strlen(argv[i]) + 3; // for start/end quote + space/null term
 	}
 
@@ -87,12 +84,13 @@ void invoke_with_merged_argv(const char *nro_path, int argc, char* argv[]) {
 	offset = strlen(nro_path);
 	merged_argv[offset++] = ' ';
 
-	for(int i = 1; i < argc; i++) {
+	for (int i = 1; i < argc; i++) {
 		merged_argv[offset++] = '"';
 		strcpy(merged_argv + offset, argv[i]);
 		offset += strlen(argv[i]);
 		merged_argv[offset++] = '"';
-		if(i != argc-1) merged_argv[offset++] = ' ';
+		if (i != argc - 1)
+			merged_argv[offset++] = ' ';
 	}
 	merged_argv[offset] = 0;
 
@@ -101,25 +99,23 @@ void invoke_with_merged_argv(const char *nro_path, int argc, char* argv[]) {
 	envSetNextLoad(nro_path, merged_argv);
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
 	socketInitializeDefault();
 	nxlinkStdio();
 
 	const char *main_pack = NULL;
-	for(int i = 0; i < argc; i++)
-	{
-		if(strcmp(argv[i], "--main-pack") == 0 && i != argc-1) {
-			main_pack = argv[i+1];
+	for (int i = 0; i < argc; i++) {
+		if (strcmp(argv[i], "--main-pack") == 0 && i != argc - 1) {
+			main_pack = argv[i + 1];
 		}
 	}
 
-	if(main_pack) {
+	if (main_pack) {
 		FILE *pack_file = fopen(main_pack, "rb");
-		if(pack_file) {
+		if (pack_file) {
 			uint32_t magic = read_32(pack_file);
 
-			if(magic != PACK_HEADER_MAGIC) {
+			if (magic != PACK_HEADER_MAGIC) {
 				bail("invalid pck magic!");
 			}
 			// pack format version, should be 1
@@ -129,28 +125,28 @@ int main(int argc, char* argv[])
 			uint32_t ver_minor = read_32(pack_file);
 			uint32_t ver_patch = read_32(pack_file); // patch number, not used for validation.
 
-			if(version != 1) {
+			if (version != 1) {
 				bail("invalid pck format!");
 			}
 
 			printf("pck for godot version %i.%i.%i\n", ver_major, ver_minor, ver_patch);
 
 			// 16 * 4 bytes reserved
-			fseek(pack_file, 16*4, SEEK_CUR);
+			fseek(pack_file, 16 * 4, SEEK_CUR);
 
 			uint32_t n_files = read_32(pack_file);
 
 			struct pack_file_entry file_entry;
-			memset(&file_entry, 0, sizeof(struct pack_file_entry)); 
+			memset(&file_entry, 0, sizeof(struct pack_file_entry));
 
 			char *custom_editor_id = NULL;
 			bool found_custom_editor_id = false;
 			// file table
-			for(int i = 0; i < n_files; i++) {
+			for (int i = 0; i < n_files; i++) {
 				read_file_entry(pack_file, &file_entry);
 				printf("got a file %s\n", file_entry.filename);
 
-				if(strcmp(file_entry.filename, "custom_editor_id") == 0) {
+				if (strcmp(file_entry.filename, "custom_editor_id") == 0) {
 					found_custom_editor_id = true;
 					custom_editor_id = malloc(file_entry.size + 1);
 					fseek(pack_file, file_entry.offset, SEEK_SET);
@@ -167,37 +163,34 @@ int main(int argc, char* argv[])
 
 			bool found_nro = false;
 			char filename_buff[256];
-			filename_buff[255]=0;
+			filename_buff[255] = 0;
 
 			char getcwd_buff[100];
 			getcwd(getcwd_buff, 100);
 
-			if(found_custom_editor_id) {
+			if (found_custom_editor_id) {
 				snprintf(filename_buff, 255, "%s/godot-%s.nro", getcwd_buff, custom_editor_id);
-				if(file_exists(filename_buff)) {
+				if (file_exists(filename_buff)) {
 					invoke_with_merged_argv(filename_buff, argc, argv);
 					found_nro = true;
 				}
-			}
-			else {
+			} else {
 				// add the cwd here because we need an absolute path later...
 				snprintf(filename_buff, 255, "%s/godot-%i.%i.%i.nro", getcwd_buff, ver_major, ver_minor, ver_patch);
 
-				if(file_exists(filename_buff)) {
+				if (file_exists(filename_buff)) {
 					invoke_with_merged_argv(filename_buff, argc, argv);
 					found_nro = true;
-				}
-				else {
+				} else {
 					int patch = ver_patch + 1;
-					while(patch--) {
-						if(patch != 0) {
+					while (patch--) {
+						if (patch != 0) {
 							snprintf(filename_buff, 255, "%s/godot-%i.%i.%i.nro", getcwd_buff, ver_major, ver_minor, patch);
-						} 
-						else {
+						} else {
 							snprintf(filename_buff, 255, "%s/godot-%i.%i.nro", getcwd_buff, ver_major, ver_minor);
 						}
 
-						if(file_exists(filename_buff)) {
+						if (file_exists(filename_buff)) {
 							invoke_with_merged_argv(filename_buff, argc, argv);
 							found_nro = true;
 							break;
@@ -206,25 +199,23 @@ int main(int argc, char* argv[])
 				}
 			}
 
-			if(!found_nro) {
+			if (!found_nro) {
 				char bail_reason[256];
-				bail_reason[255]=0;
-				if(found_custom_editor_id) {
+				bail_reason[255] = 0;
+				if (found_custom_editor_id) {
 					snprintf(bail_reason, 255, "Failed to find a compatible Godot version, wanted a custom build '%s'!", custom_editor_id);
-				}
-				else {
+				} else {
 					snprintf(bail_reason, 255, "Failed to find a compatible Godot version, wanted %i.%i.%i!", ver_major, ver_minor, ver_patch);
 				}
 				bail(bail_reason);
 			}
 
-			if(found_custom_editor_id) free(custom_editor_id);
-		}
-		else {
+			if (found_custom_editor_id)
+				free(custom_editor_id);
+		} else {
 			bail("Failed to open PCK!");
 		}
-	}
-	else {
+	} else {
 		bail("Failed to find main_pack argument!");
 	}
 
@@ -232,6 +223,6 @@ int main(int argc, char* argv[])
 	fflush(stderr);
 
 	socketExit();
-    
-    return 0;
+
+	return 0;
 }
